41f9bb0db15af2d7d039746e528803c17aa94f52
wslider <wslider2000@gmail.com>
2025-09-23 10:31:36 -0400

    added js kata

--- kata.js
+++ kata.js
@@ -6,3 +6,24 @@ function digitalRoot(n) {
   else if (n % 9 === 0) return 9;
   else return n % 9;
 }
+
+
+// Character Counter - 2025 September 23
+
+function validateWord(s)
+{
+ const newArray = Array.from(s.toLowerCase());
+  
+  // Count occurrences of each character
+  const counts = {};
+  for (const item of newArray) {
+    counts[item] = (counts[item] || 0) + 1;
+  }
+  
+  // Get the set of counts (unique frequencies)
+  const frequencies = Object.values(counts);
+  
+  // Check if all frequencies are the same
+  return frequencies.every(count => count === frequencies[0]);
+  
+}
